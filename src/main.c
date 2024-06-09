64010978e073ab3b387cd2e5041de6c6fdbf28f2
Gomaink <muka5458@gmail.com>
2024-06-09 04:51:13 -0300

    structure changes

--- src/main.c
+++ src/main.c
@@ -13,7 +13,7 @@ int main() {
 
     DWORD processId = GetProcessIdByName(processName);
     if (processId == 0) {
-        fprintf(stderr, "GTA: San Andreas não está em execução.\n");
+        fprintf(stderr, "GTA: San Andreas nao esta em execução.\n");
         return 1;
     }
 
@@ -31,18 +31,17 @@ int main() {
 
     float playerHealth = GetPlayerHealth(hProcess);
     float playerMaxHealth = GetPlayerMaxHealth(hProcess);
-    printf("A vida do jogador é: %.2f/%.2f\n", playerHealth, playerMaxHealth);
+    printf("A vida do jogador e: %.2f/%.2f\n", playerHealth, playerMaxHealth);
 
     float playerArmour = GetPlayerArmour(hProcess);
-    printf("O colete do jogador é: %.2f\n", playerArmour);
-
+    printf("O colete do jogador e: %.2f\n", playerArmour);
 
     float x = ReadFloat(hProcess, X_ADDRESS);
     float y = ReadFloat(hProcess, Y_ADDRESS);
     float z = ReadFloat(hProcess, Z_ADDRESS);
-    printf("A localização do jogador é: X = %.2f, Y = %.2f, Z = %.2f\n", x, y, z);
+    printf("A localizacao do jogador e: X = %.2f, Y = %.2f, Z = %.2f\n", x, y, z);
 
-    printf("A localização do jogador é: %s\n", getPlayerZone(x, y, z));
+    printf("A localizacao do jogador e: %s\n", getPlayerZone(x, y, z));
 
     CloseHandle(hProcess);
     return 0;
